5598b9bd598bdfe2fc8357a7c56d8c3490248d95
periyappa <periyappa464@gmail.com>
2025-11-03 03:01:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,34 @@ st.write('The name on your Smoothie will be:' ,name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect_
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
+)
+if ingredients_list:
+    
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    combined_value = ingredients_string + " (" + name_on_order + ")"
+
+    my_insert_stmt = f"""
+       INSERT INTO smoothies.public.orders(ingredients)
+    VALUES ('{combined_value}')
+"""
+
+    st.success(f"✅ Your Smoothie is ordered, {name_on_order}!")
+
+    st.write(my_insert_stmt)
+    st.stop()
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+       session.sql(my_insert_stmt).collect()
+       st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display smoothiefroot nutrition information
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
