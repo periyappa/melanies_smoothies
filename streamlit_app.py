3ae83063c9ec3222c65624d61e083ebdb0008b33
periyappa <periyappa464@gmail.com>
2025-11-02 23:30:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
        INSERT INTO smoothies.public.orders(ingredients)
     VALUES ('{combined_value}')
 """
-  import requests
+import requests
        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     # st.text(smoothiefroot_response.json())   
        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
