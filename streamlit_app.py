990f8171bf759b4d99c0d823a46a8cb90db37f7f
periyappa <periyappa464@gmail.com>
2025-11-03 03:47:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,27 @@
 import streamlit as st
-from snowflake.snowpark.functions import col
+import pandas as pd
+import requests
 
 st.title("🍓 Customize Your Smoothie!")
 
-# Text input for smoothie name
-name_on_order = st.text_input("Name on Smoothie:")
-st.write("The name on your Smoothie will be:", name_on_order)
+st.write("Choose the fruits you want in your custom Smoothie!")
 
-# Load fruit options from Snowflake
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+name_on_smoothie = st.text_input("Name on Smoothie:")
 
-# Display dataframe to verify the SEARCH_ON column
-st.dataframe(data=my_dataframe, use_container_width=True)
+st.write("The name on your Smoothie will be:", name_on_smoothie)
 
-# Stop execution here temporarily
-st.stop()
+ingredients_list = st.multiselect("Choose up to 5 ingredients:", 
+                                  ["Avocado", "Banana", "Blackberry", "Blueberry", 
+                                   "Cherry", "Coconut", "Fig", "Grape", 
+                                   "Guava", "Kiwi", "Lemon", "Lime", 
+                                   "Mango", "Orange", "Papaya", "Peach", 
+                                   "Pineapple", "Pomegranate", "Raspberry", 
+                                   "Strawberry", "Tangerine", "Watermelon", "Ximenia", "Yerba Mate"])
 
-# Multiselect to choose ingredients
-ingredients_list = st.multiselect(
-    "Choose up to 5 ingredients:",
-    my_dataframe
-)
+if ingredients_list:
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefruit_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefruit_response.json(), use_container_width=True)
