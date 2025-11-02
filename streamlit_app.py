076a6deb7da2b482b80efd57b471da95a1f0a1e2
periyappa <periyappa464@gmail.com>
2025-11-02 23:34:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,9 @@ if ingredients_list:
     VALUES ('{combined_value}')
 """
 import requests
-       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    # st.text(smoothiefroot_response.json())   
-       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
     st.success(f"✅ Your Smoothie is ordered, {name_on_order}!")
 
     
