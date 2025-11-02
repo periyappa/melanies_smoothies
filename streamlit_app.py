7d412d6e7c72bdb7ad48a2d1d39434135c1bcd36
periyappa <periyappa464@gmail.com>
2025-11-03 03:39:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,23 @@
 import streamlit as st
-import pandas as pd
-import requests
+from snowflake.snowpark.functions import col
 
 st.title("🍓 Customize Your Smoothie!")
 
-st.write("Choose the fruits you want in your custom Smoothie!")
+# Text input for smoothie name
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_on_order)
 
-name_on_smoothie = st.text_input("Name on Smoothie:")
+# Load fruit options from Snowflake
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-st.write("The name on your Smoothie will be:", name_on_smoothie)
+# Display dataframe to verify the SEARCH_ON column
+st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect("Choose up to 5 ingredients:", 
-                                  ["Avocado", "Banana", "Blackberry", "Blueberry", 
-                                   "Cherry", "Coconut", "Fig", "Grape", 
-                                   "Guava", "Kiwi", "Lemon", "Lime", 
-                                   "Mango", "Orange", "Papaya", "Peach", 
-                                   "Pineapple", "Pomegranate", "Raspberry", 
-                                   "Strawberry", "Tangerine", "Watermelon", "Ximenia", "Yerba Mate"])
+# Stop execution here temporarily
+st.stop()
 
-if ingredients_list:
-    ingredients_string = ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefruit_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefruit_response.json(), use_container_width=True)
+# Multiselect to choose ingredients
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:",
+    my_dataframe
+)
