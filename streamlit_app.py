e348cb77ea23ae51afed46dd7896ea879ad58fce
periyappa <periyappa464@gmail.com>
2025-11-03 04:07:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 # Get fruit options from Snowflake
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 # Multiselect box for ingredients
